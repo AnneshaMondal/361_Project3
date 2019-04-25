58d81d6d280dd5142345ddd901e4f8c6ee9c06d2
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-25 18:47:25 -0500

    pin image added, map javascript added

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,22 @@
 function initMap(){
-  var map= new google.maps.Map(document.getElementById('map'), {
-    center:{lat:41.9250, lng:87.6339},
-    zoom:25
+
+  var position={lat:41.9250, lng:87.6339};
+  var map= new google.maps.Map(document.getElementsById('map'), {center:position, zoom:15};
+
+  var view=new google.maps.InfoWindow({
+    content:'Caldwell Lily Pond, Lincoln Park Zoo'
+
   });
 
-  var
+  var marker=new google.maps.Marker({
+    position:position,
+    map:map,
+    icon:'images/pin.png'
+  });
 
+  marker.addListener('click', function(){
+    view.open(map,marker);
 
-}
+  });
+
+});
