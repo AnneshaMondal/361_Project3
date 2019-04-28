6880b1ce40d10136acf947a800cf40db30eaade3
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-28 18:23:38 -0500

    west town image added for slider

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,25 @@ function initMap(){
   });
 }
 window.addEventListener('load', initMap);
+
+
+var slideCounter= 1;
+showSlides(slideCounter);
+
+function plusSlides(n) {
+  showSlides((slideCounter += n));
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideCounter = 1;
+  }
+  if (n < 1) {
+    slideCounter= slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
