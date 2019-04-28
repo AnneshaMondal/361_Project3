288cc3850800fdb54fc0fd9b164313f67fe1e37e
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-28 18:10:06 -0500

    bracket added

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 function initMap(){
 
   var position={lat:41.9250, lng:87.6339};
-  var map= new google.maps.Map(document.getElementsById('map'), {center:position, zoom:15};
+  var map= new google.maps.Map(document.getElementsById('map'), {center:position, zoom:15});
 
 
 
