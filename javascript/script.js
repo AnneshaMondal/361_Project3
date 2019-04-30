9b1379b1dca152079126b18ea0cbf51276b3e3af
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-30 17:02:39 -0500

    lng value corrected

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 41.9250, lng: 87.6339},
+		center: {lat: 41.9250, lng: -87.6339},
 		zoom: 8
 	});
 }
