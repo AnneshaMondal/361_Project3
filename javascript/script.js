ceba23fe7a5b5fe3bba1cfc76d3440de6be4f619
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-30 16:54:05 -0500

    javascript slider added

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,25 @@ function initMap() {
 		zoom: 8
 	});
 }
+
+
+var slideCounter = 1;
+slides();
+
+function addSlide(n) {
+  slides((slideCounter += n));
+}
+function slides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideCounter = 1;
+  }
+  if (n < 1) {
+    slideCounter = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideCounter - 1].style.display = "block";
+}
